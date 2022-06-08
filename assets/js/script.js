4320da6121d7d2bbef7bb3f775b335726bdc568e
Jessica Wang <jessica.l.wang01@gmail.com>
2022-06-08 14:35:40 -0400

    adds function to save search, edits styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,10 @@
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-// WHEN I view current weather conditions for that city
-// the date, an icon representation of weather conditions
-
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
+let previousSearches = [];
 
 var getLatLon = function (city) {
 
@@ -28,6 +20,9 @@ var getLatLon = function (city) {
         // get city name and insert into element
         city = data.name;
         $('#current-city').text(city)
+
+        // save name of city searched
+        saveSearch(city)
 
         // get lat and lon coordinates for more compreshensive api call, which will be used to get data
         lat = data.coord.lat;
@@ -63,7 +58,9 @@ function getCityWeather(lat, lon) {
 }
 
 function insertCurrentWeather(data) {
+  var image_source = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png"
   $('#current-temp').text(data.temp)
+  $('#current-icon').attr("src", image_source)
   $('#current-wind').text(data.wind_speed)
   $('#current-humidity').text(data.humidity)
   $('#current-uv').text(data.uvi)
@@ -73,15 +70,29 @@ function insertForecast(data) {
   console.log(data)
  
   for (i = 1; i < 6; i++){
-    console.log(data[i])
     var date = new Date(data[i].dt*1000)
     var date_formatted = (date.getMonth()+1) + "/" +  date.getDate()  + "/" +  date.getFullYear().toString().substr(-2);
+    var image_source = "https://openweathermap.org/img/wn/" + data[i].weather[0].icon + ".png"
     $('#forecast-date-' + i).text(date_formatted)
+    $('#forecast-icon-' + i).attr("src", image_source)
     $('#forecast-temp-' + i).text(data[i].temp.day)
     $('#forecast-wind-' + i).text(data[i].wind_speed)
     $('#forecast-humidity-' + i).text(data[i].humidity)
   }
 }
+
+function saveSearch(city){
+  previousSearches.push(city)
+  const historyDiv = document.getElementById('search-history');
+  const newButton = document.createElement("button");
+  newButton.innerText = city
+  historyDiv.appendChild(newButton)
+}
+
+// function createButton{
+  
+// }
+
 
 $('#search-button').on('click', function () {
 
